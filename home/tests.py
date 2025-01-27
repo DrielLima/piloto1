864828b325000455f42b9d9d7a5d249137a674ba
DrielLima <160731830+DrielLima@users.noreply.github.com>
2025-01-27 11:22:57 -0300

    Update tests.py

--- home/tests.py
+++ home/tests.py
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-from django.test import TestCase
 
-# Create your tests here.
-=======
 from django.test import TestCase
-
-# Create your tests here.
->>>>>>> e84f4e8dfe6c3fa3fcee23e7a5092c49c0a68f24
