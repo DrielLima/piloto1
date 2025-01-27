674d712f9a034d5e616a07e0c5549fe6708db217
DrielLima <160731830+DrielLima@users.noreply.github.com>
2025-01-27 11:01:26 -0300

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
+
 from django.db import models
 
 # Create your models here.
 =======
 from django.db import models
-
-# Create your models here.
->>>>>>> e84f4e8dfe6c3fa3fcee23e7a5092c49c0a68f24
