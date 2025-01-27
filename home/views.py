571b09b340c8777de7ece3b9435332f5c2e2732d
DrielLima <160731830+DrielLima@users.noreply.github.com>
2025-01-27 11:23:42 -0300

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-# Create your views here.
+
 from django.shortcuts import render, HttpResponse
 
 def index(request):
@@ -9,16 +8,3 @@ def sobre(request):
 def contato(request):
     return render(request, 'contato.html') 
 def ajuda (request): 
-=======
-# Create your views here.
-from django.shortcuts import render, HttpResponse
-
-def index(request):
-    return render(request, 'index.html')
-def sobre(request):
-    return render(request, 'sobre.html')
-def contato(request):
-    return render(request, 'contato.html') 
-def ajuda (request): 
->>>>>>> e84f4e8dfe6c3fa3fcee23e7a5092c49c0a68f24
-    return render(request, 'ajuda.html')
